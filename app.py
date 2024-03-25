0c787135df8aa6480115e53ac21c84493185f228
AJorja <158013141+AJorja@users.noreply.github.com>
2024-03-25 05:07:17 +0000

    Website

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  
+    return render_template("index.html", title="Homepage")
+
+@app.route('/about')
+def about():  
+    return render_template("about.html", title="About Us")
+
+@app.route('/contact')
+def contact():  
+    return render_template("contact.html", title="Contact Us")
+
+@app.route('/photogallery') 
+def photogallery(): 
+    return render_template("photogallery.html", title="Photo Gallery")
 
 
 if __name__ == '__main__':
